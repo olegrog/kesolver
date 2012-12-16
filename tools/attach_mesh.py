fa46db05cca3178f959e40f3490126700f73d1f6
dodulad <dodulad@list.ru>
2012-12-16 17:22:51 +0400

    attach_mesh.py added

---
+++ tools/attach_mesh.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import json
+
+if __name__ == "__main__":
+
+    # open .kem file
+    with open(sys.argv[-2], 'rb') as fd:
+        meshdata = json.load(fd)
+
+    # open .kep file
+    with open(sys.argv[-3], 'rb') as fd:
+        data = json.load(fd)
+
+
+    # attach
+    data['mesh'] = meshdata
+        
+    # dump in .kei file
+    with open(sys.argv[-1], 'wb') as fd: 
+        json.dump(data, fd)
+
