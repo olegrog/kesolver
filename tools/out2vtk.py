25a91330d1768f0b434b411d848c0240f788b976
Oleg Rogozin <o.a.rogozin@gmail.com>
2014-12-05 14:46:38 +0300

    output all macro in vtk file

--- tools/out2vtk.py
+++ tools/out2vtk.py
@@ -3,6 +3,7 @@
 import sys, numpy
 import re
 import out2
+import numpy as np
 
 def gmshTypeToVTKType(gmshtype):
     VTKTypes = [-1, -1, -1, -1, 10, 12, 13]
@@ -13,6 +14,23 @@ def listToStr(elm):
 
 def list3ToStr(l):
     return ' '.join( map(str, l) )
+
+def toVec(data, pos):
+    return map(list, zip(*data[pos : pos + 3]))
+
+def writeScalar(name, data, pos):
+    for i, scalars in enumerate(data[pos::N]):
+        fd.writelines("SCALARS " + name + "%d float\n" % i)
+        fd.writelines("LOOKUP_TABLE default\n")
+        for scalar in scalars:
+            fd.writelines(str(scalar) + '\n')
+
+def writeVector(name, data, pos):
+    for i, vectors_x in enumerate(data[pos::N]):
+        vectors = toVec(data, pos+i*N)
+        fd.writelines("VECTORS " + name + "%d float\n" % i)
+        for vector in vectors:
+            fd.writelines(list3ToStr(vector) + '\n')
 
 nodes, cells = out2.readNodesCells(sys.argv[1])
 data = out2.readMacros(sys.argv[2], len(cells))
@@ -38,34 +56,18 @@ with open(sys.argv[3], "w") as fd:
 
     fd.writelines("CELLS %d %d\n" % ( len(cells), sum( [len(cell.nodes)+1 for cell in cells] ) ))
     for cell in cells:
-        fd.writelines(listToStr(cell.nodes)+'\n')
+        fd.writelines(listToStr(cell.nodes) + '\n')
 
     fd.writelines("CELL_TYPES %d\n" % len(cells))
     for cell in cells:
-        fd.writelines(str(gmshTypeToVTKType(cell.type))+'\n')
+        fd.writelines(str(gmshTypeToVTKType(cell.type)) + '\n')
 
     fd.writelines("CELL_DATA %d\n" % len(cells))
 
-    for i, n in enumerate(data[0::8]):
-        fd.writelines("SCALARS n%d float 1\n" % i)
-        fd.writelines("LOOKUP_TABLE default\n")
-        for density in n:
-            fd.writelines(str(density)+'\n')
-
-    for i, t in enumerate(data[4::8]):
-        fd.writelines("SCALARS t%d float\n" % i)
-        fd.writelines("LOOKUP_TABLE default\n")
-        for temperature in t:
-            fd.writelines(str(temperature)+'\n')
-
-    fd.writelines("POINT_DATA %d\n" % (len(nodes)+len(centernodes)))
-    for i, vx in enumerate(data[1::8]):
-        v = data[1+i*8:4+i*8]
-        fd.writelines("VECTORS v%d float\n" % i)
-        for i in range(len(nodes)):
-            fd.writelines('0. 0. 0.\n')
-        for velocity in v:
-            fd.writelines(list3ToStr(velocity)+'\n')
-
-
-
+    writeScalar('Density', data, 0)
+    writeVector('Velocity', data, 1)
+    writeScalar('Temperature', data, 4)
+    writeVector('Temperatures', data, 5)
+    writeVector('HeatFlux', data, 8)
+    writeVector('ShearStress', data, 11)
+    writeScalar('H-function', data, 14)
